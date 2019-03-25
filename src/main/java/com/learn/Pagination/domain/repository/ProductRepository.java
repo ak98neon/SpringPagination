e27d69a6f0e086ecf16af7f2141bf03ab03c701c
Artem Kudria <akudria@axiomsl.com>
2019-03-25 16:36:53 +0200

    Create product controller, re-order repository, create products page, create product service with findAll, save, findAllByPrice

--- src/main/java/com/learn/Pagination/domain/repository/ProductRepository.java
+++ src/main/java/com/learn/Pagination/domain/repository/ProductRepository.java
@@ -1,13 +1,16 @@
 package com.learn.Pagination.domain.repository;
 
 import com.learn.Pagination.domain.entity.Product;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author Artem Kudria
  */
+@Repository
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
-    Page<Product> findAllByPrice(Double price, Pageable pageable);
+    List<Product> findAllByPrice(Double price, Pageable pageable);
 }
