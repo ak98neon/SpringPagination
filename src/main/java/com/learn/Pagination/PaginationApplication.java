5d9adbc6e86e73863393a18fc9489775d6ae4bd3
Artem Kudria <akudria@axiomsl.com>
2019-03-25 17:40:11 +0200

    Add search files into entry point

--- src/main/java/com/learn/Pagination/PaginationApplication.java
+++ src/main/java/com/learn/Pagination/PaginationApplication.java
@@ -2,12 +2,16 @@ package com.learn.Pagination;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.learn")
+@EnableJpaRepositories(basePackages = "com.learn")
 public class PaginationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaginationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaginationApplication.class, args);
+    }
 
 }
